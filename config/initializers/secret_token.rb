50fa6d82d2997b7b7d2f18689eb8ee710b8286c3
star <xing.yang@taodinet.com>
2013-04-04 14:59:30 +0800

    #create new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoveHouse::Application.config.secret_token = 'a9a127a9790e19da96e52068acfc82ea78237a61c55a105f0cea27aa82dffa3af3bc4f0be25da5e87f0e9cefbedd74123cfc066b108b383725cb62d33dae12a2'
